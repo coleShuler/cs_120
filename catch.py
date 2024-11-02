b82387e1857c2d41528a674676ffa867f59b7b82
coleShuler <cole.shuler@bsu.edu>
2024-11-01 21:19:25 -0700

    Stage 1 of the game :O

--- catch.py
+++ catch.py
@@ -3,15 +3,24 @@ import pygame, simpleGE, random
 class Turtle(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("turtle.jpg")
+        self.setImage("turtle.png")
+        self.setSize(100, 50)
+        self.position = (320, 400)
+        self.moveSpeed = 3
         
+    def process(self):
+        if self.isKeyPressed(pygame.K_LEFT):
+            self.x -= self.moveSpeed
+        if self.isKeyPressed(pygame.K_RIGHT):
+            self.x += self.moveSpeed
+            
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("leaf.jpg")
         self.turtle = Turtle(self)
         
-        self.sprite = [self.turtle]
+        self.sprites  = [self.turtle]
         
 def main():
     game = Game()
